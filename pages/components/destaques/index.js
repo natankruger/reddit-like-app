8147868e713be65f82eafe87142616a3d6e7dc98
Gustavo alex vieira <gustavoalxvieira@gmail.com>
2022-09-12 19:23:45 -0300

    Adicionado estilo para os botões de destaques

--- pages/components/destaques/index.js
+++ pages/components/destaques/index.js
@@ -1,11 +1,12 @@
 import Style from './style.module.css'
+import BtnStyle from './buttonStyle.module.css'
 
 export default function Destaques() {
     return (<section className={Style.Wrapper}>
-        <button>Melhor</button>
-        <button>Em Destaque</button>
-        <button>Novo</button>
-        <button>Mais votados</button>
-        <button>Extras</button>
+        <button className={BtnStyle.NeonButton}>Melhor</button>
+        <button className={BtnStyle.NeonButton}>Em Destaque</button>
+        <button className={BtnStyle.NeonButton}>Novo</button>
+        <button className={BtnStyle.NeonButton}>Mais votados</button>
+        <button className={BtnStyle.NeonButton}>Extras</button>
     </section>)
   }
